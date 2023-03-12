de14cfd647b982699d2c1d3a23fb9646cddf161f
hhbbjjya <109157661+hhbbjjya@users.noreply.github.com>
2023-03-12 13:18:59 +0800

    Add files via upload

---
+++ important.py
@@ -0,0 +1,7 @@
+high=input("請輸入身高")
+weight=input("請輸入體重")
+fat=input("請輸入體脂肪")
+year=input("清輸入年齡")
+muscles=input("請輸入肌肉")
+gender=input("男性請輸入1女性請輸入0")
+consumption=99.9* weight +6.25* high +4.92*year +gender*166
