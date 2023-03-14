5e0f47c02ea346793a5127ca5b31748e8dd8430d
hhbbjjya <109157661+hhbbjjya@users.noreply.github.com>
2023-03-14 16:17:55 +0800

    Add files via upload

---
+++ finally.py
@@ -0,0 +1,20 @@
+import openai
+openai.api_key = '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'
+def qus(x):
+    #x = str(input("請問有甚麼問題?"))
+    response = openai.Completion.create(
+    engine = "text-davinci-003",    # select model
+    prompt = x,     
+    max_tokens = 512,               # response tokens
+    temperature = 1,                # diversity related
+    top_p = 0.75,                   # diversity related
+    n = 1,                          # num of response
+)
+
+    completed_text = response["choices"][0]["text"]
+    return(completed_text)
+    
+
+
+x = str(input("請問有甚麼問題?"))
+print(qus(x))
